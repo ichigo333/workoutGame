e57bdad9add77a23e783eb001bfa469b0b58c9a1
Alex <alex333sh@gmail.com>
2022-08-02 02:32:48 -0400

    setup
    - setup of Model
    - setup of Tests

---
+++ workoutGame.ModelTests/ExerciseTests.cs
@@ -0,0 +1,14 @@
+namespace workoutGame.ModelTests {
+    [TestClass]
+    public class ExerciseTests {
+        [TestInitialize]
+        public void Setup() {
+            var pushups = new ExerciseTests();
+        }
+        
+        [TestMethod]
+        public void TestMethod1() {
+        
+        }
+    }
+}
---
+++ workoutGame.Models/Exercise.cs
@@ -0,0 +1,25 @@
+﻿namespace workoutGame.Models {
+    
+    
+    public class Exercise {
+        private string Name { get; set; }
+        private int Points { get; set; }
+        private int armsPercent { get; set; }
+        private int chestPercent { get; set; }
+        private int absPercent { get; set; }
+        private int backPercent { get; set; }
+        private int legsPercent { get; set; }
+
+        public Exercise() {
+            
+        }
+    }
+}
+
+
+//    self.points = points
+//    self.arms = arms
+//    self.chest = chest
+//    self.abs = abs
+//    self.back = back
+//    self.legs = legs
